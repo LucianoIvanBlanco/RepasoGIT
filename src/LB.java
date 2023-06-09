618272000d2da5a5cb72458644c239a0f253a8a3
Luciano Blanco <luciano.blanco@live.u-tad.com>
2023-06-09 13:15:07 +0200

    Modificacion 2

--- src/LB.java
+++ src/LB.java
@@ -4,6 +4,7 @@ public class LB {
 	public static void main(String[] args) {
 		System.out.println("Alumno: Luciano Blanco");
 		System.out.println("Modificacion desde Master");
+		System.out.println("Modificacion desde Develop");
 	}
 
 }
